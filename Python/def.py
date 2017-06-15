829798171c0d2dbf9a4c4228eea70177ffc79bf5
Li-Liu <li.liu@cn.dlink.com>
2017-06-15 17:36:07 +0800

    def name():

---
+++ Python/def.py
@@ -0,0 +1,16 @@
+#模块
+
+
+def def1 ():
+	print('this is a empty def')
+	
+def def2(a,b):
+	return a + b
+	
+def free(a,b):
+	for i in range(a,b+1):
+		print(i)
+		
+def1()
+print(def2(1,2))
+print(free(1,5))
